521c82e65c30082e2c3006dbc15039283573ba4f
Syammed2429 <43319549+Syammed2429@users.noreply.github.com>
2024-12-04 00:45:39 +0800

    feat: add LanguageSelect component for language selection in header

---
+++ src/components/header/languages-select.tsx
@@ -0,0 +1,29 @@
+import { dropDownLanguages } from "@/utils/data/json-data";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+
+interface LanguageSelectProps {
+    value: string;
+    onChange: (value: string) => void;
+}
+
+export const LanguageSelect = ({ value, onChange }: LanguageSelectProps) => {
+    const selectValue = value || 'all';
+
+    const handleValueChange = (newValue: string) => {
+        onChange(newValue === 'all' ? '' : newValue);
+    };
+    return (
+        <Select value={selectValue} onValueChange={handleValueChange}>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select Language" />
+            </SelectTrigger>
+            <SelectContent>
+                {dropDownLanguages.map((language) => (
+                    <SelectItem key={language.value} value={language.value}>
+                        {language.label}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    );
+}
